986008a84fb90dfc212cc8db3604ffb1663f10a7
Albert Clemente <55829242+albertclementesq@users.noreply.github.com>
2021-06-12 11:39:13 +0200

    Update widgettoolkit.js

--- widgettoolkit.js
+++ widgettoolkit.js
@@ -4,41 +4,42 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.tampermonkey.net/index.php?version=4.11.6120&ext=fire&updated=true
+// @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-        'use strict';
+    'use strict';
 
-        if (typeof(Sequra) == 'undefined') {
-            console.log('No se ha cargado SeQura');
-            return;
-        }
-        if (typeof(SequraHelper) == 'undefined') {
-            console.log('No se ha cargado SeQuraHelper');
-            return;
-        }
+    if (typeof(Sequra) !== 'undefined') {
+        console.log('Sequra cargado!');
+    }
+
+    if (typeof(SequraHelper) !== 'undefined') {
+        console.log('SequraHelper cargado!');
+        
         SequraHelper.original_drawPromotionWidget = SequraHelper.drawPromotionWidget;
         SequraHelper.drawPromotionWidget = function(price_src, dest, product, theme, reverse, campaign) {
             if (typeof(SequraHelper.WidgetsPintados) == 'undefined') {
-                SequraHelper.WidgetsPintados = []
+                SequraHelper.WidgetsPintados = [];
             }
             SequraHelper.WidgetsPintados[product] = {
                 src: price_src,
                 dest: dest
             }
+
             console.log('Widget ' + product + ' pintado.');
             console.log(document.querySelectorAll(price_src).length + ' elementos con el selector de precio');
             console.log(document.querySelectorAll(dest).length + ' elementos con el selector de destino');
             SequraHelper.original_drawPromotionWidget(price_src, dest, product, theme, reverse, campaign);
         }
+
         SequraHelper.listWidgets = function() {
             document.querySelectorAll('.sequra-promotion-widget').forEach(
                 function(item, index) {
                     console.log(item.getAttribute('data-product') + ' observes ->' + item.getAttribute('observes'));
                 }
-            )
+            );
         }
 
         SequraHelper.drawPromotionWidget_original = SequraHelper.drawPromotionWidget;
@@ -50,10 +51,10 @@
 
         /*
             SequraHelper.drawPromotionWidget = function (price_src,dest,product,theme,reverse,campaign) {
-              SequraHelper.drawPromotionWidget_original(price_src,dest,product,theme,reverse,campaign);
-              SequraHelper.reDrawnWidgetsByProduct[product] = function () {
-                 SequraHelper.reDrawWidgetsByProduct(price_src,dest,product,theme,reverse,campaign);
-              }
+            SequraHelper.drawPromotionWidget_original(price_src,dest,product,theme,reverse,campaign);
+            SequraHelper.reDrawnWidgetsByProduct[product] = function () {
+                SequraHelper.reDrawWidgetsByProduct(price_src,dest,product,theme,reverse,campaign);
+            }
             }
         */
         SequraHelper.removeWidgetByProduct = function(product) {
@@ -63,5 +64,6 @@
                 }
             );
         }
+
     }
-)();
+})();
